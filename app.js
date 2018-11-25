4c34f7c7128935ff00f177125939f8c2d00f5ff1
hee-jong.lee@oracle.com <hee-jong.lee@oracle.com>
2018-11-25 17:35:30 +0900

    hello world

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Oracle Cloud Native Application with Heejong and Sangdeuk');
+  res.send('Hello World');
 });
 
 app.listen(3000, function () {
