56bb00d60d8e0b77bf5418bff941ef0e8d12c442
hee-jong.lee@oracle.com <hee-jong.lee@oracle.com>
2018-11-26 10:41:32 +0900

    add message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello World and Oracle ');
 });
 
 app.listen(3000, function () {
