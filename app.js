1755dd3c2dbb05ed815709f5d59846bc8a04eca8
isheejong <hee-jong.lee@oracle.com>
2019-01-24 22:03:26 +0900

    oracle digital prime

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Heejong ');
+  res.send('Hello World Oracle Digital Primee Heejong ');
 });
 
 app.listen(3000, function () {
