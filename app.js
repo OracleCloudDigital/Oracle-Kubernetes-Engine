318d03c1061b0ffd6185fff53a15faab42d98b34
Oracle Digital Prime Korea <44891733+odpKr@users.noreply.github.com>
2019-01-28 10:06:12 +0900

    Update app.js
    Heejong

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Oracle Digital Prime');
+  res.send('Hello World Oracle Digital Prime Heejong');
 });
 
 app.listen(3000, function () {
