a75249490f18e255a537510e9138358b418cb4ff
Oracle Digital Prime Korea <44891733+odpKr@users.noreply.github.com>
2019-02-13 14:17:46 +0900

    Update app.js
    delete a name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Oracle Digital Prime Heejong');
+  res.send('Hello World Oracle Digital Prime ');
 });
 
 app.listen(3000, function () {
